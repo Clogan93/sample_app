5d295174abb6b19a1b38ea3a53e59f7aac566b04
Logan Craig <logan@grandpc.org>
2017-04-01 03:41:18 -0700

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,20 @@
+Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'pages/home'
+
+  get 'pages/contact'
+
+  get 'pages/about'
+
+  get 'pages/help'
+
+  match '/signup',	:via => [:get], :to => 'users#new'
+
+  match '/contact', :via => [:get], :to => 'pages#contact'
+  match '/about', 	:via => [:get], :to => 'pages#about'
+  match '/help', 	:via => [:get], :to => 'pages#help'
+
+  root :via => [:get], :to => 'pages#home'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
